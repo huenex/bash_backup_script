6540052c96b7e366799bbec3e1485988931385ae
huenex <ulric_@live.com>
2017-02-10 09:23:05 -0800

    Update and rename script.sh to bash_home_backup.sh

--- bash_home_backup.sh
+++ bash_home_backup.sh
@@ -11,11 +11,10 @@ usrdir=~
 spacr=":"
 directory=$fsl$bufname$fsl
 bufname="HelloWorld"
-timestamp=`date +%Y-%m-%d:%H:%M:%S`
+timestamp=`date +%Y%m%d+%j`
 sumkdir=/BACKUP_TARS/
 fil="./location"
-
-
+tmpfile="./tmpfile"
 #root/sudo only
 if [ ! -e $fil ]; then
 	echo $cwd >> $fil
@@ -81,15 +80,52 @@ echo -e "\e[31m2:\e[0m Manage Directories"
 echo -e "\e[31m3:\e[0m Exit"
 read bashboivarsneak
 if [ $bashboivarsneak -eq 1 ]; then
-	echo -e "Enter the name of the desired destination for home backup.\n Do not enter the full path, just the name of the folder. ie 'folder_1' "
-	echo "List of backup folders"
+	cd $HOME
+	echo $timestamp >> $tmpfile
+	echo -e "Enter the name of the desired destination for home backup.\nDo not enter the full path, just the name of the folder. ie 'folder_1'\n "
+	echo -e "List of backup folders\n"
 	cd $sumkdir
 	pwd
 	ls -lah
-	read folderchosen
+	echo -e "\nEnter folder name below:"
+	read folderchosen	
 	cd $HOME
-	tar -czvf $timestamp.tar.gz /BACKUP_TARS/${!folderchosen}
-	else	
+	tar -cf /BACKUP_TARS/$folderchosen/$timestamp.tar ./*
+        echo $timestamp >> $tmpfile
+	tmp=$(head -n 1 tmpfile)
+	if [ -e $tmpfile ]; then
+		#cd $line
+        	sudo rm -rf ./tmpfile
+		#cd $HOME
+	fi
+	echo -e "\nSelect Chown/Chmod for tar archive\n"
+	echo "Please select permissions for USER (chmod, 0-7)"
+	read usrchmd
+	echo "Please select permissions for GROUP (chmod, 0-7)"
+	read grpchmd
+	echo "Please select permissions for PUBLIC (chmod, 0-7)"
+	read pubchmd
+	echo "Please select user for chown"
+	read usrchown
+	echo "Please select group for chown"
+	read grpchown
+	cd /BACKUP_TARS/$folderchosen/
+	echo -e "\nchown $usrchown$spacr$grpchown"
+	echo "chmod $usrchmd$grpchmd$pubchmd"
+	chown $usrchown$spacr$grpchown $tmp.tar
+	chmod $usrchmd$grpchmd$pubchmd $tmp.tar
+	echo -e "\nComplete.\n"
+	ls -lah
+	echo -e "\nBackup Complete."
+	sleep 1
+	echo -e "\nWould you like to continue using the bash home backup tool?\n1: Yes\n2:No"
+	read continewe
+	if [ $continewe -eq 1 ]; then
+		cd $line
+		'./bash_home_backup.sh'
+		exit
+	fi
+fi
 		if [ $bashboivarsneak -eq 3 ]; then
 		echo "Goodbye."
 		exit 0		
@@ -98,11 +134,12 @@ if [ $bashboivarsneak -eq 1 ]; then
 			echo "What action would you like to perform?"
 			echo -e "\e[31m1:\e[0m Create subfolder"
 			echo -e "\e[31m2:\e[0m List subfolders"
-			echo -e "\e[31m3:\e[0m Exit"
+			echo -e "\e[31m3:\e[0m Chown subfolder"
+			echo -e "\e[31m4:\e[0m Exit"
 			read fldrmenu
 
 			if [ $fldrmenu -eq 1 ]; then
-				echo "Please enter the name of the folder"
+			echo "Please enter the name of the folder"
 				read fldrmenu_createname
 				echo "Please select permissions for USER (chmod, 0-7)"
 				read usrchm
@@ -127,25 +164,46 @@ if [ $bashboivarsneak -eq 1 ]; then
 				cd $line
 				'./bash_home_backup.sh'	
 			fi
+			if [ $fldrmenu -eq 3 ]; then
+				echo "Please enter the name of the folder"
+				read fldrmenu_createname
+				echo "Please select permissions for USER (chmod, 0-7)"
+				read usrchm
+				echo "Please select permissions for GROUP (chmod, 0-7)"
+				read grpchm
+				echo "Please select permissions for PUBLIC (chmod, 0-7)"
+				read pubchm
+				echo "Please select user for chown"
+				read usrchwn
+				echo "Please select group for chown"
+				read grpchwn
+				cd /BACKUP_TARS/
+				echo "Changing permissions for $fldrmenu_createname"
+				chmod $usrchm$grpchm$pubchm $fldrmenu_createname
+				echo "chmod $usrchm$grpchm$pubchm"
+				echo "chown $usrchwn$spacr$grpchwn"
+				chown $usrchwn$spacr$grpchwn $fldrmenu_createname
+				echo "Complete."
+				sleep 1
+				clear
+				cd $line
+				'./bash_home_backup.sh'	
+			fi
 		fi
 		if [ $fldrmenu -eq 2 ]; then
-			echo "Listing subfolders of /BACKUP_TARS/"
+			echo -e "\n"
+			echo  "Listing subfolders of /BACKUP_TARS/"
 			cd /BACKUP_TARS/
 			ls -lah
-			echo "Hit Enter to Continue >"
+			echo -e "\nHit Enter to Continue >"
 			read cwd
 			sleep 1
 			clear
 			cd $line
 			'./bash_home_backup.sh'
 		fi
-		if [ $fldrmenu -eq 3 ]; then
-			echo "Goodbye"
-			sleep 1
-			exit 0
-		else
-			clear
-			cd $line
-			'./bash_home_backup.sh'
-		fi
+		echo "Goodbye"
+		sleep 1
+		clear
+		exit
 fi
